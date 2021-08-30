e4e0810a39577580a5f00aeb37443e5e71ff4fe4
Suparoek <39317424+Suparoek@users.noreply.github.com>
2021-08-30 10:02:23 +0700

    recipe1

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products!:Product[];
+  constructor() {
+      this.products=[];
+      this.products.push({
+        name: 'ส้มโอ',
+        price: 900
+      });
+      this.products.push({
+        name: 'แตงโม',
+        price: 1000
+      });
+      this.products.push({
+        name: 'มะพร้าวนำ้หอม',
+        price: 500
+      });
+   }
+
+  ngOnInit(): void {
+  }
+
+}
